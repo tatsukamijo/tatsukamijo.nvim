5485b71a5e4013beb8bf437b6588942ff6539e51
Tatsuya Kamijo <tatsukamijo@icloud.com>
2026-01-06 02:36:07 +0900

    fix: minor fix on telescope version

--- init.lua
+++ init.lua
@@ -435,7 +435,7 @@ require('lazy').setup({
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    -- branch = '0.1.x', -- Neovim 0.11+ needs main branch
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
