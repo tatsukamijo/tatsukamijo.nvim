836e3d3e0dc4ca197d25447999249ba04cabcb66
Tatsuya Kamijo <tatsukamijo@icloud.com>
2026-01-06 01:12:56 +0900

    style: fix height of small terminal for claude's diff to work well

--- init.lua
+++ init.lua
@@ -220,6 +220,7 @@ vim.keymap.set('n', '<space>st', function()
   vim.cmd.term()
   vim.cmd.wincmd 'J'
   vim.api.nvim_win_set_height(0, 10)
+  vim.wo.winfixheight = true
 end)
 
 vim.keymap.set('n', '<space>sv', function()
